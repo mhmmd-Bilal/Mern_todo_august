719c392caf5f419460e093f07c159e41c40c47ae
Muhammed Bilal <muhammedbilal6211@gmail.com>
2026-05-12 14:51:26 +0530

    added middlewares

--- backend/server.js
+++ backend/server.js
@@ -7,5 +7,20 @@ let port = 4000
 
 connectDb()
 
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
+// app.httpMethod(route,handler)
+
+// app.get('/getAll/users',(req,res)=>{
+//     let person ={
+//         name :"sam",
+//         age : 18
+//     }
+//     res.send(person)
+// })
+
+
+// http://localhost:4000
 
 app.listen(port , ()=>console.log("server started"))
